3254d941df14fd5c8743139020eeaeca9d7f7309
Yulia Suslova <suslova.dzhulia2017@yandex.ru>
2022-09-04 12:11:07 +0300

    get patient appointment

--- src/main/java/org/saturn/clinicscheduler/controller/AppointmentController.java
+++ src/main/java/org/saturn/clinicscheduler/controller/AppointmentController.java
@@ -1,9 +1,12 @@
 package org.saturn.clinicscheduler.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.saturn.clinicscheduler.model.dto.response.AppointmentResponseDto;
 import org.saturn.clinicscheduler.service.AppointmentService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,8 +21,21 @@ public class AppointmentController {
 
     @PostMapping
     public ResponseEntity<AppointmentResponseDto> createAnAppointment(@RequestParam Long patientId,
-                                                                      @RequestParam Long scheduleId) {
+        @RequestParam Long scheduleId) {
         return ResponseEntity.ok(appointmentService.createAnAppointment(patientId, scheduleId));
+    }
+
+    @GetMapping("/patients/{id}")
+    public ResponseEntity<List<AppointmentResponseDto>> getPatientAppointments(
+        @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(appointmentService.getAllAppointmentsByPatient(id));
+    }
+
+    @GetMapping("/doctors/{id}")
+    public ResponseEntity<List<AppointmentResponseDto>> getDoctorAppointments(
+        @PathVariable Long id) {
+        return ResponseEntity.ok(appointmentService.getAllAppointmentsByDoctor(id));
     }
 
 }
---
+++ src/main/java/org/saturn/clinicscheduler/model/dto/request/AppointmentRequestDto.java
@@ -0,0 +1,18 @@
+package org.saturn.clinicscheduler.model.dto.request;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppointmentRequestDto {
+
+    private Long patientId;
+    private Long doctorId;
+    private LocalDate date;
+    private LocalTime time;
+}
