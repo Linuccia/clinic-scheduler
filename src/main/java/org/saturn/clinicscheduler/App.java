970b1035aae5b8a82af735169024b4d38f3eba29
Anton <Antosah1@mail.ru>
2022-09-06 22:30:07 +0300

    added swagger config and bug fix

--- src/main/java/org/saturn/clinicscheduler/App.java
+++ src/main/java/org/saturn/clinicscheduler/App.java
@@ -1,7 +1,14 @@
 package org.saturn.clinicscheduler;
 
 import org.saturn.clinicscheduler.config.Config;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
 
 public class App extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -15,7 +22,30 @@ public class App extends AbstractAnnotationConfigDispatcherServletInitializer {
     }
 
     @Override
+    public void onStartup(ServletContext servletContext) {
+        WebApplicationContext context = getContext();
+        servletContext.addListener(new ContextLoaderListener(context));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("RestServlet",
+                new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/*");
+    }
+
+    @Override
     protected String[] getServletMappings() {
-        return new String[]{"/"};
+        return new String[] {"/"};
+    }
+
+    private AnnotationConfigWebApplicationContext getContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.scan("org.aston");
+        context.register(Config.class, org.springdoc.webmvc.ui.SwaggerConfig.class,
+                org.springdoc.core.SwaggerUiConfigProperties.class, org.springdoc.core.SwaggerUiOAuthProperties.class,
+                org.springdoc.webmvc.core.SpringDocWebMvcConfiguration.class,
+                org.springdoc.core.MultipleOpenApiSupportCondition.class,
+                org.springdoc.core.SpringDocConfiguration.class, org.springdoc.core.SpringDocConfigProperties.class,
+                org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration.class);
+
+        return context;
     }
 }
