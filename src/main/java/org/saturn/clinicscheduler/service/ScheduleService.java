0267edfb3f25a5611a97d714428a23e9977cfb51
RST595 <rst5995@gmail.com>
2022-09-16 08:03:56 +0300

    draft

--- src/main/java/org/saturn/clinicscheduler/service/ScheduleService.java
+++ src/main/java/org/saturn/clinicscheduler/service/ScheduleService.java
@@ -2,6 +2,7 @@ package org.saturn.clinicscheduler.service;
 
 import org.saturn.clinicscheduler.model.dto.request.ScheduleUnpartitionedDto;
 import org.saturn.clinicscheduler.model.dto.response.ScheduleResponseDto;
+import org.saturn.clinicscheduler.model.entity.Schedule;
 
 import java.util.List;
 
@@ -11,4 +12,5 @@ public interface ScheduleService {
 
     List<ScheduleResponseDto> getAllDoctorSchedules(Long doctorId);
 
+    ScheduleResponseDto changeSchedule(Long id, ScheduleUnpartitionedDto scheduleUnpartitionedDto);
 }
