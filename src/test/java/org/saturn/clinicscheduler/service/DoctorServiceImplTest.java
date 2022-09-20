6a27290f8abd81b5672961c59a9a47448b9431d6
ilin-ei <ilin.ei@yandex.ru>
2022-09-20 16:51:39 +0300

    add doctor tests

--- src/test/java/org/saturn/clinicscheduler/service/DoctorServiceImplTest.java
+++ src/test/java/org/saturn/clinicscheduler/service/DoctorServiceImplTest.java
@@ -1,5 +1,6 @@
 package org.saturn.clinicscheduler.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.saturn.clinicscheduler.exception.DoctorNotFoundException;
 import org.saturn.clinicscheduler.mapper.DoctorMapper;
 import org.saturn.clinicscheduler.mapper.SpecialityMapper;
 import org.saturn.clinicscheduler.model.dto.request.DoctorCreateDto;
@@ -19,6 +21,8 @@ import org.saturn.clinicscheduler.repository.DoctorRepository;
 import org.saturn.clinicscheduler.repository.SpecialityRepository;
 import org.saturn.clinicscheduler.service.impl.DoctorServiceImpl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -37,16 +41,12 @@ public class DoctorServiceImplTest {
 
     @Mock
     DoctorRepository doctorRepository;
-
     @Mock
     DoctorMapper doctorMapper;
-
     @Mock
     SpecialityRepository specialityRepository;
-
     @Mock
     SpecialityMapper specialityMapper;
-
     @InjectMocks
     DoctorServiceImpl doctorService;
 
@@ -72,18 +72,50 @@ public class DoctorServiceImplTest {
     void createDoctor() {
 
         Mockito.when(doctorRepository.save(doctor)).thenReturn(doctor);
-
         Mockito.when(doctorMapper.mapToDoctor(doctorCreateDto, speciality)).thenReturn(doctor);
-
         Mockito.when(doctorMapper.mapToInfoDto(doctor)).thenReturn(doctorInfoDto);
-
         Mockito.when(specialityRepository.findById(1L)).thenReturn(Optional.ofNullable(speciality));
-
         DoctorInfoDto result = doctorService.createDoctor(doctorCreateDto);
-
         assertEqualsRequestAndResponse(doctorCreateDto, result);
-
         Mockito.verify(doctorRepository, Mockito.times(1)).save(doctor);
+    }
+
+    @Test
+    void getListOfDoctors() {
+
+        doctorService.getAllDoctors();
+        Mockito.verify(doctorRepository, Mockito.times(1)).findAll();
+    }
+
+    @Test
+    void getListOfDoctorsOfChosenSpeciality() {
+
+        List<Doctor> doctors = new ArrayList<>();
+        doctors.add(doctor);
+        Mockito.when(specialityRepository.findById(1L)).thenReturn(Optional.ofNullable(speciality));
+        Mockito.when(doctorRepository.getDoctorsBySpecialityId(1L)).thenReturn(doctors);
+        Mockito.when(doctorMapper.mapToInfoDto(doctor)).thenReturn(doctorInfoDto);
+        List<DoctorInfoDto> result = doctorService.getDoctorsBySpecialityId(1L);
+        for(DoctorInfoDto d: result){
+            assertEquals(speciality.getName(), d.getSpeciality());
+        }
+        Mockito.verify(doctorRepository, Mockito.times(1)).getDoctorsBySpecialityId(1L);
+    }
+
+    @Test
+    void deleteDoctorById() {
+
+        Mockito.when(doctorRepository.findById(1L)).thenReturn(Optional.ofNullable(doctor));
+        doctorService.deleteDoctor(1L);
+        Mockito.verify(doctorRepository, Mockito.times(1)).deleteById(1L);
+    }
+
+    @Test
+    void throwExceptionOnWrongDoctorId() {
+
+        Mockito.when(doctorRepository.findById(10L)).thenThrow(new DoctorNotFoundException());
+        Assertions.assertThrows(DoctorNotFoundException.class, () -> { doctorService.deleteDoctor(10L); });
+        Mockito.verify(doctorRepository, Mockito.times(0)).deleteById(10L);
     }
 
 }
