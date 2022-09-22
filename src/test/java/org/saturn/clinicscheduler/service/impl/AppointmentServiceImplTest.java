eaa4052d4c731ce4186c239fc4d247fc447834b1
Rostislavskii Maksim <95303168+RST595@users.noreply.github.com>
2022-09-22 20:43:17 +0300

    SSEP-2_test (#28)

--- src/test/java/org/saturn/clinicscheduler/service/impl/AppointmentServiceImplTest.java
+++ src/test/java/org/saturn/clinicscheduler/service/impl/AppointmentServiceImplTest.java
@@ -7,16 +7,26 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.saturn.clinicscheduler.mapper.AppointmentMapper;
+import org.saturn.clinicscheduler.mapper.PatientMapper;
+import org.saturn.clinicscheduler.model.EntityGeneratorTest;
+import org.saturn.clinicscheduler.model.dto.response.AppointmentResponseDto;
+import org.saturn.clinicscheduler.model.dto.response.PatientInfoDto;
 import org.saturn.clinicscheduler.model.entity.Appointment;
+import org.saturn.clinicscheduler.model.entity.Patient;
 import org.saturn.clinicscheduler.model.entity.Schedule;
 import org.saturn.clinicscheduler.repository.AppointmentRepository;
+import org.saturn.clinicscheduler.repository.PatientRepository;
 import org.saturn.clinicscheduler.repository.ScheduleRepository;
 
 import java.sql.Date;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.saturn.clinicscheduler.model.EntityGeneratorTest.*;
 import static org.saturn.clinicscheduler.model.EntityGeneratorTest.appointment;
+import static org.saturn.clinicscheduler.model.EntityGeneratorTest.patient;
+import static org.saturn.clinicscheduler.model.EntityGeneratorTest.patientInfoDto;
 import static org.saturn.clinicscheduler.model.EntityGeneratorTest.schedule;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,16 +36,43 @@ class AppointmentServiceImplTest {
     private ScheduleRepository scheduleRepository;
     @Mock
     private AppointmentRepository appointmentRepository;
+    @Mock
+    private PatientRepository patientRepository;
+    @Mock
+    private PatientMapper patientMapper;
+    @Mock
+    private AppointmentMapper appointmentMapper;
     @InjectMocks
     private AppointmentServiceImpl appointmentService;
 
     private Appointment appointment;
     private Schedule schedule;
+    private Patient patient;
+    private PatientInfoDto patientInfoDto;
+    private AppointmentResponseDto appointmentResponseDto;
 
     @BeforeEach
     void init() {
         appointment = appointment();
         schedule = schedule();
+        patient = patient();
+        patientInfoDto = patientInfoDto();
+        appointmentResponseDto = appointmentResponseDto();
+    }
+
+    @Test
+    void createAppointment() {
+        Mockito.when(patientRepository.findById(1L)).thenReturn(Optional.of(patient));
+        Mockito.when(scheduleRepository.findById(1L)).thenReturn(Optional.of(schedule));
+        Mockito.when(patientMapper.toInfoDto(patient)).thenReturn(patientInfoDto);
+        Mockito.when(appointmentRepository.save(appointment)).thenReturn(appointment);
+        Appointment appointmentWithoutId = appointment;
+        appointmentWithoutId.setId(null);
+        Mockito.when(appointmentMapper.toResponseDto(appointment, schedule, patientMapper.toInfoDto(patient)))
+                .thenReturn(appointmentResponseDto);
+        appointmentService.createAppointment(1l, 1L);
+        Mockito.verify(scheduleRepository, Mockito.times(1)).save(schedule);
+        Mockito.verify(appointmentRepository, Mockito.times(1)).save(appointmentWithoutId);
     }
 
     @Test
